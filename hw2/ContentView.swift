9241d1dec14a2a8f4680aa1c6e11e4ded40b9278
c60603 <c60603@gmail.com>
2020-10-19 16:00:06 +0800

    Initial Commit

---
+++ hw2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hw2
+//
+//  Created by User08 on 2020/10/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ hw2/hw2App.swift
@@ -0,0 +1,17 @@
+//
+//  hw2App.swift
+//  hw2
+//
+//  Created by User08 on 2020/10/19.
+//
+
+import SwiftUI
+
+@main
+struct hw2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
